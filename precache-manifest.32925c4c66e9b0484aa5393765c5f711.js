c9ff0334f9acccc8aaac9bab305056e04cb34a2e
Maryna <maritkacheva@hotmail.com>
2019-09-17 21:50:07 +0300

    Updates

---
+++ precache-manifest.32925c4c66e9b0484aa5393765c5f711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b828007933af0ebb28091d43a72c1524",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3f0ff2f82722dd1673c5",
+    "url": "/goit-react-hw-01-components/static/css/main.083140ab.chunk.css"
+  },
+  {
+    "revision": "c7175c2f98df4da1dad2",
+    "url": "/goit-react-hw-01-components/static/js/2.d345fe00.chunk.js"
+  },
+  {
+    "revision": "3f0ff2f82722dd1673c5",
+    "url": "/goit-react-hw-01-components/static/js/main.42e02686.chunk.js"
+  },
+  {
+    "revision": "ea7e4364a76cfd99a9ac",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.ae7fc8ff.js"
+  }
+]);
